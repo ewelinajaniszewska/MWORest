b5428f4d66a70acb9139ddc802c9b61ae3b29143
student4 <student4@pcoip.ki.agh.edu.pl>
2018-05-13 15:45:55 +0200

    poczatkiMeetingu

---
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -0,0 +1,75 @@
+package com.company.enroller.controllers;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.company.enroller.model.Meeting;
+import com.company.enroller.persistence.MeetingService;
+
+@RestController
+@RequestMapping("/meetings")
+public class MeetingRestController {
+
+	@Autowired
+	MeetingService meetingService;
+
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public ResponseEntity<?> getMeetings() {
+		Collection<Meeting> meetings = meetingService.getAll();
+		return new ResponseEntity<Collection<Meeting>>(meetings, HttpStatus.OK);
+	}
+	
+	
+	/*@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> getMeeting(@PathVariable("id") String login) {
+	     Participant participant = participantService.findByLogin(login);
+	     if (participant == null) {
+	         return new ResponseEntity(HttpStatus.NOT_FOUND);
+	     }
+	     return new ResponseEntity<Participant>(participant, HttpStatus.OK);
+	 }
+	
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	 public ResponseEntity<?> addParticipant(@RequestBody Participant participant){
+		//czy nie istnieje
+		if (participantService.findByLogin(participant.getLogin())!=null) {
+			return new ResponseEntity(HttpStatus.CONFLICT);
+		}
+		participantService.addParticipant(participant);
+		return new ResponseEntity<Participant>(participant, HttpStatus.OK);
+		//dodac
+		//zwrocic
+		
+}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	 public ResponseEntity<?> deleteParticipant(@PathVariable("id") String login){
+		 Participant participant = participantService.findByLogin(login);
+		 if (participant == null) {
+			 return new ResponseEntity(HttpStatus.NOT_FOUND);
+		 }
+		  participantService.deleteParticipant(participant);
+		  return new ResponseEntity<Participant>(participant, HttpStatus.OK);
+			
+}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	 public ResponseEntity<?> updateParticipant(@PathVariable("id") String login, @RequestBody Participant updatedParticipant ){
+		 Participant participant = participantService.findByLogin(login);
+		 if (participant == null) {
+			 return new ResponseEntity(HttpStatus.NOT_FOUND);
+		 }
+		 participant.setPassword(updatedParticipant.getPassword());
+		  participantService.updateParticipant(participant);
+		  return new ResponseEntity<Participant>(participant, HttpStatus.OK);
+}*/
+}
+
